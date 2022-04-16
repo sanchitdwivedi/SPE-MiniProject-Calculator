2b922e50aad6312c480ef8d7234b2acc6c82ecd8
unknown <sanchitdwivedi123@gmail.com>
2022-04-16 13:53:14 +0530

    Final UI.

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,58 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-calculator',
-  templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.css']
+	selector: 'app-calculator',
+	templateUrl: './calculator.component.html',
+	styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+	ngOnInit(): void {
+	}
+	
+	value: string = "0";
+	value1: number = 0;
+	value2: number = 0;
+	expOprn: boolean = false;
+	performed: boolean = false;
+	
+	
+	pressNum(num: number) {
+		if(this.performed){
+			this.value1 = num;
+			this.performed = false;
+			return;
+		}
+		if (this.expOprn) {
+			this.value1 = 0;
+		}
+		this.value1 = (this.value1 * 10) + num;
+	}
+	
+	allClear() {
+		this.value1 = 0;
+		this.value2 = 0;
+		this.value = this.value1.toString();
+	}
+	
+	getAnswer() {
+		if (this.expOprn) {
+			this.value1 = Math.pow(this.value2, this.value1);
+			this.value = this.value1.toString();
+		} else {
+			this.value1 = 0;
+			this.value = this.value1.toString();
+		}
+		this.expOprn = false;
+		this.performed = true;
+	}
 
-  answer:any = '';
+	exp() {
+		this.value2 = this.value1
+		this.expOprn = true;
+	}
 
-  constructor() { }
+	fact() {
+		if(Number.isInteger(this.value1)){
+			this.value1 = this.factorial(this.value1);
+		}else if(this.value1>800){
+			this.value = "Infinity";
+			this.value1 = 0;
+		}else {
+			this.value = "Invalid Number";  
+		}
+		this.performed = true;
+	}
 
-  ngOnInit(): void {
-  }
+	factorial(n: number): any {
+		if (n == 0 || n == 1)
+			return 1;
+		return this.factorial(n - 1) * n;
+	}
 
-  calculate(){
-    //Getting the number
-    let temp:any = document.getElementById("number");
-    let number = parseInt(temp.value);
-    console.log(typeof number);
+	root() {
+		this.value1 = Math.sqrt(this.value1);
+		this.performed = true;
+		this.value = this.value1.toString();
+	}
 
-    //Getting the selector
-    let e:any = document.getElementById("sel1");
-    let selectValue = e.value;
-    console.log(selectValue);
-   
-    // Sqrt
-    if(selectValue === "1"){
-       this.answer=Math.sqrt(number);
-    }
-
-    // Factorial
-    if(selectValue === "2"){
-      this.answer= this.sFact(number);
-    }
-    
-    // Natural Logerithm
-    if(selectValue === "3"){
-      this.answer= Math.log(number);
-    }
-    
-    // Power function
-    if(selectValue === "4"){
-      this.answer= Math.pow(number, 3);
-    }
-    
-  }
-
-  sFact(num:number)
-  {
-    var rval=1;
-    for (var i = 2; i <= num; i++)
-        rval = rval * i;
-    return rval;
-  }
+	ln() {
+		this.value1 = Math.log(this.value1);
+		this.performed = true;
+		this.value = this.value1.toString();
+	}
 
 }
