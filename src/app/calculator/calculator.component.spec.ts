ac63b521cb4859b6762f4c7d699ce39b68b287d6
unknown <sanchitdwivedi123@gmail.com>
2022-04-17 10:10:57 +0530

    Added: Test cases in Calculator component

--- src/app/calculator/calculator.component.spec.ts
+++ src/app/calculator/calculator.component.spec.ts
@@ -22,4 +22,50 @@ describe('CalculatorComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("Testing input as  1", () => {
+    const cal = fixture.componentInstance;
+    cal.pressNum(1);
+    expect(cal.value1).toEqual(1);
+  })
+  
+  it("Testing input as 15", () => {
+    const cal = fixture.componentInstance;
+    cal.pressNum(1);
+    cal.pressNum(5);
+    expect(cal.value1).toEqual(15);
+  })
+  
+  it("Testing Root of 144", () => {
+    const cal = fixture.componentInstance;
+    cal.pressNum(1);
+    cal.pressNum(4);
+    cal.pressNum(4);
+    cal.root()
+    expect(cal.value1).toEqual(12);
+  })
+  
+  it("Testing Factorial of 5", () => {
+    const cal = fixture.componentInstance;
+    cal.pressNum(5);
+    cal.fact()
+    expect(cal.value1).toEqual(120);
+  })
+  
+  it("Testing Exponential of 7 to power 2", () => {
+    const cal = fixture.componentInstance;
+    cal.pressNum(7);
+    cal.exp();
+    cal.pressNum(2);
+    cal.getAnswer()
+    expect(cal.value1).toEqual(49);
+  })
+  
+  it("Testing Log of 50", () => {
+    const cal = fixture.componentInstance;
+    cal.pressNum(5);
+    cal.pressNum(0);
+    cal.ln();
+    expect(cal.value1).toEqual(3.912023005428146);
+  })
 });
